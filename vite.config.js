a397b12e3685535485af6c4bed8a66855a7f9354
Oscar Choque <oscarch0qu3@gmail.com>
2024-12-02 10:01:31 -0500

    chore: chunks

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,27 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ["react", "react-dom"], // Divide librerías grandes
+
+          // Agrupa las librerías de Radix UI
+          radixUI: [
+            "@radix-ui/react-alert-dialog",
+            "@radix-ui/react-avatar",
+            "@radix-ui/react-checkbox",
+            "@radix-ui/react-dialog",
+            "@radix-ui/react-dropdown-menu",
+            "@radix-ui/react-icons",
+            "@radix-ui/react-label",
+            "@radix-ui/react-popover",
+            "@radix-ui/react-select",
+            "@radix-ui/react-separator",
+            "@radix-ui/react-slot",
+            "@radix-ui/react-switch",
+            "@radix-ui/react-tabs",
+          ],
+          // Agrupa librerías de utilidades
+          utilities: ["axios", "clsx", "js-cookie", "zod"],
+          // Librerías de características específicas
+          features: ["react-router-dom", "recharts", "react-spinners"],
         },
       },
     },
